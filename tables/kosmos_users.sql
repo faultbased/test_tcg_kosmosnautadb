661eb100cb94145a24b1e7230002c7f64d7c3daa
faultbased <s_cammorto@outlook.com>
2022-04-25 17:10:18 -0400

    appended .sql extension

---
+++ tables/kosmos_users.sql
@@ -0,0 +1,7 @@
+CREATE TABLE kmUsers (
+    userUID uuid NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4 (),
+    userName VARCHAR(50) NOT NULL,
+    passW VARCHAR(150) NOT NULL,
+    accountStatus SMALLINT(1) NOT NULL,
+    PRIMARY KEY (userUID)
+);
